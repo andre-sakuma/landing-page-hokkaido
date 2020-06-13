c4d442cf0307c659d2428c0e5c54ba9bacc86ea6
André Yuji Sakuma <andre.sakuma@tenda.digital>
2020-06-13 20:49:52 -0300

    add first commit

---
+++ landing-page/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-content>
+    <div>
+      teste
+    </div>
+  </v-content>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      teste: 'oi'
+    }),
+  }
+</script>
+
+<style scoped>
+  div{
+    background: blue;
+  }
+</style>
---
+++ landing-page/src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-content>
+    <div class="base">
+      <div class="teste">
+        <p>oi</p>
+      </div>
+    </div>
+  </v-content>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+
+    data: () => ({
+      teste: 'oi'
+    }),
+  }
+</script>
+
+<style scoped>
+  .base{
+    background: red;
+  }
+  .teste{
+    margin: 0 auto;
+    background: gray;
+  }
+  p{
+    margin: auto;
+  }
+</style>
